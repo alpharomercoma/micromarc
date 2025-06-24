2a7cb8ab7cebbb7a2658714dbe3815947bd2b8c5
Alpha Romer Coma <alpharomercoma@proton.me>
2025-06-24 18:57:05 +0800

    Add openGraph metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,14 @@ export const metadata: Metadata = {
   description: "Visual-Qwen consists of a frozen CLIP ViT-L/14 vision encoder and a Whisper V3 Turbo audio transcription module, a lightweight Query-Former (Q-Former), and a frozen Qwen3-4B large language model.",
   keywords: "Visual-Qwen, Qwen, multimodal AI, attention mechanism, sludge content, short form videos",
   authors: [{ name: "Alpha Romer Coma" }, { name: "Marc Olata" }, { name: "Job Isaac Ong" }, { name: "Kristoffer Ian Sioson" }],
+  openGraph: {
+    images: ["/images/theoretical_framework.png"],
+    title: "Visual-Qwen: Augmenting Multimodal Deep Learning with Attention Mechanisms to Recognize 'Sludge Content' from Short Form Videos",
+    description: "Visual-Qwen consists of a frozen CLIP ViT-L/14 vision encoder and a Whisper V3 Turbo audio transcription module, a lightweight Query-Former (Q-Former), and a frozen Qwen3-4B large language model.",
+    siteName: "Visual-Qwen",
+    type: "website",
+    url: "https://micromarc.me",
+  },
 };
 
 export default function RootLayout({
